2670e6c60f8c2ada6a967af79c27d8df8577dc47
unknown <copolat@yahoo.comcopolat git config --global user.name copolat>
2020-12-27 14:22:02 -0600

    Added event handler outside of the function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent } from '*.svg';
+import React from 'react';
+import Button from './Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.appName}</h1>;
+        <Button title='Click Me'/>;
+      </div>
+      
+    );
+  }
+
 }
 
 export default App;
