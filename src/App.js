268033bf465d1e7180d4aa12348994588db2e5db
unknown <copolat@yahoo.comcopolat git config --global user.name copolat>
2020-12-30 19:21:57 -0600

    Remove unnecassary components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React from 'react';
-import Button from './Button';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state ={
-      color:'rgb(0,0,0)'
-    }
-    this.buttonClicked = this.buttonClicked.bind(this);
-  }
 
-  randomNumber(){
-    return Math.floor(Math.random() *255) + 1;
-  }
-
-  buttonClicked(){
-    const redColor = this.randomNumber();
-    const greenColor = this.randomNumber();
-    const blueColor = this.randomNumber();
-    const rgbCode = `rgb(${redColor},${greenColor},${blueColor})`
-      // document.getElementById('header').style.color =rgbCode;
-      document.getElementById('paragraph').innerText =rgbCode;
-      // document.getElementById('paragraph').style.color =rgbCode;
-      this.setState({color:rgbCode})
-    }
-  render() {
+function App() {
     return (
       <div>
-        <h1 id="header" style={{color: this.state.color}}>{this.props.appName}</h1>
-        <p id="paragraph" style={{color: this.state.color}}>{this.props.description}</p>
-        <Button title='Click Me' click={this.buttonClicked}/>
+        <h1>TO-DO LIST</h1>
       </div>
   );
-  }
-
 }
 
 export default App;
--- src/Button.js
+++
@@ -1,5 +0,0 @@
-function Button (props){
-    return <button onClick={props.click}>{props.title}</button>
-}
-
-export default Button;
